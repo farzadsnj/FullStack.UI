d65cb723ce8b431495a2a01b4a4ef29ee9db7889
Farzad Sanjarani <farzadsanjarani3@gmail.com>
2023-06-10 15:52:49 +1000

    co4

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -7,56 +7,56 @@ import { Employee } from 'src/app/models/employee.model';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-  employees: Employee[] = [
-    {
-    id: '1',
-    name:'Farzad Sanjarani',
-    email:'farzadsnjau@gmail.com',
-    phone: 415479443,
-    salary: 6000,
-    department: 'Human Resources'
-  },
-  {
-    id: '12',
-    name:'Farzad Sanjarani',
-    email:'farzadsnjau@gmail.com',
-    phone: 415479443,
-    salary: 6000,
-    department: 'Human Resources'
-  },
-  {
-    id: '123',
-    name:'Farzad Sanjarani',
-    email:'farzadsnjau@gmail.com',
-    phone: 415479443,
-    salary: 6000,
-    department: 'Human Resources'
-  },
-  {
-    id: '1234',
-    name:'Farzad Sanjarani',
-    email:'farzadsnjau@gmail.com',
-    phone: 415479443,
-    salary: 6000,
-    department: 'Human Resources'
-  },
-  {
-    id: '12345',
-    name:'Farzad Sanjarani',
-    email:'farzadsnjau@gmail.com',
-    phone: 415479443,
-    salary: 6000,
-    department: 'Human Resources'
-  },
-  {
-    id: '123456',
-    name:'Farzad Sanjarani',
-    email:'farzadsnjau@gmail.com',
-    phone: 415479443,
-    salary: 6000,
-    department: 'Human Resources'
-  }
-]
+  employees: Employee[] = []
+//     {
+//     id: '1',
+//     name:'Farzad Sanjarani',
+//     email:'farzadsnjau@gmail.com',
+//     phone: 415479443,
+//     salary: 6000,
+//     department: 'Human Resources'
+//   },
+//   {
+//     id: '12',
+//     name:'Farzad Sanjarani',
+//     email:'farzadsnjau@gmail.com',
+//     phone: 415479443,
+//     salary: 6000,
+//     department: 'Human Resources'
+//   },
+//   {
+//     id: '123',
+//     name:'Farzad Sanjarani',
+//     email:'farzadsnjau@gmail.com',
+//     phone: 415479443,
+//     salary: 6000,
+//     department: 'Human Resources'
+//   },
+//   {
+//     id: '1234',
+//     name:'Farzad Sanjarani',
+//     email:'farzadsnjau@gmail.com',
+//     phone: 415479443,
+//     salary: 6000,
+//     department: 'Human Resources'
+//   },
+//   {
+//     id: '12345',
+//     name:'Farzad Sanjarani',
+//     email:'farzadsnjau@gmail.com',
+//     phone: 415479443,
+//     salary: 6000,
+//     department: 'Human Resources'
+//   },
+//   {
+//     id: '123456',
+//     name:'Farzad Sanjarani',
+//     email:'farzadsnjau@gmail.com',
+//     phone: 415479443,
+//     salary: 6000,
+//     department: 'Human Resources'
+//   }
+// ]
 
   constructor(){}
 
